8851ae02b4a29818ebf7ebe6cefa8c0614303e16
Pranjal Tripathi <155900440+PranjalTripatHI07@users.noreply.github.com>
2025-06-21 00:00:29 +0530

    Delete Internship 2025 directory

--- Internship 2025/task1_partD.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to BMI Calculator")
-weight = float(input("\t\n Enter your weight in kilograms: "))
-
-height = float(input("\t\n Enter your height in meters: "))
-
-
-bmi = weight / (height * height)
-
-print(f"\t\n Your BMI is: {bmi}")
